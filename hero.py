cdddc32f36ec4b02d9f7c8be0591bd2d516eab38
Tarashkevich-Vladislav <74532592+Tarashkevich-Vladislav@users.noreply.github.com>
2020-12-17 21:34:39 +0300

    Create hero.py

---
+++ hero.py
@@ -0,0 +1,12 @@
+from gameunit import *
+
+class Hero(Attacker):
+    def __init__(self, name):
+        self._name = name
+        self._experience = 0
+        self._health = 100
+        self._attack = 50
+
+    def attack(self, target):
+        target._health -= self._attack
+# атака 50, здоровье 100, опыт 0, имя задаваемое, пробитие уроном на атаку
